d3b97a67efa7e2c4e1004d86d084b4cc54a7aee7
Andrew Lodes <alodes999@gmail.com>
2015-08-10 13:57:05 -0500

    Fix bug with my typing of lib/libs

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-angular.module('flapperNews', [])
-.controller('MainCtrl', [
-  '$scope',
-  function($scope){
-    $scope.test = 'Hello World!';
+angular.module("flapperNews", [])
+.controller("MainCtrl", [
+'$scope',
+function($scope){
+  $scope.test = 'Hello World!';
 }])
-
-$scope.posts = [
-  'post 1'
-  'post 2'
-  'post 3'
-  'post 4'
-  'post 5'
-]
+//
+// $scope.posts = [
+//   'post 1'
+//   'post 2'
+//   'post 3'
+//   'post 4'
+//   'post 5'
+// ]
