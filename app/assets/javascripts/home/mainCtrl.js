afa8b15b98b69853ad998073c771a14bc1a6f0af
Andrew Lodes <alodes999@gmail.com>
2015-08-12 16:36:06 -0500

    Remove commented code no longer needed

--- app/assets/javascripts/home/mainCtrl.js
+++ app/assets/javascripts/home/mainCtrl.js
@@ -11,15 +11,6 @@ function($scope, posts){
       title: $scope.title,
       link: $scope.link,
     });
-    // $scope.posts.push({
-    //   title: $scope.title,
-    //   link: $scope.link,
-    //   upvotes: 0,
-    //   comments: [
-    //     {author: 'Joe', body: 'Cool post!', upvotes: 0},
-    //     {author: 'Bob', body: 'Good idea but completely wrong', upvotes: 0}
-    //   ]
-    // });
     $scope.title = '';
     $scope.link = '';
   };
