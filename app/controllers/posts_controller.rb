3ad8d6b373db817ab2af09cf6c5cfaffae8c044d
Andrew Lodes <alodes999@gmail.com>
2015-08-12 15:34:08 -0500

    Add route handling for our needed routes in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,23 @@
 class PostsController < ApplicationController
   
+  def index
+    respond_with Post.all
+  end
+  
+  def create
+    respond_with Post.create
+  end
+  
+  def show
+    respond_with Post.find(params[:id])
+  end
+  
+  def upvote
+    post = Post.find(params[:id])
+    post.increment!(:upvotes)
+  end
+  
+  private
   
   def post_params
     params.require(:post).permit(:link, :title)
