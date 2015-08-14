1c0d4b65bd7e2adc1ca97bece1bad76aa95098ae
Andrew Lodes <alodes999@gmail.com>
2015-08-14 10:29:28 -0500

    Add states for register and login in app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -14,9 +14,8 @@ angular.module("flapperNews", ['ui.router', 'templates', 'Devise'])
             return posts.getAll();
           }]
         }
-      });
-      
-    $stateProvider  
+      })
+        
       .state('posts', {
         url: '/posts/{id}',
         templateUrl: 'posts/_posts.html',
@@ -26,8 +25,20 @@ angular.module("flapperNews", ['ui.router', 'templates', 'Devise'])
             return posts.get($stateParams.id);
           }]
         }
+      })
+      
+      .state('login', {
+        url: '/login',
+        templateUrl: 'auth/_login.html',
+        controller: 'AuthCtrl'
+      })
+      
+      .state('register', {
+        url: '/register',
+        templateUrl: 'auth/_register.html',
+        controller: 'AuthCtrl'
       });
-  
+    
     $urlRouterProvider.otherwise('home');
 }])
 
